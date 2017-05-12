bd836fe49b29a588ebc26e2540ece3e42d3b5fdd
Kalilo Jade Hansmann <kalilohansmann@gmail.com>
2017-05-12 12:07:02 +0200

    Added routes for genres and torrents

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,14 @@ Rails.application.routes.draw do
       resource :movie
       get 'movies/:id', to: 'movies#show'
       get 'movies', to: 'movies#index'
+
+      resource :genre
+      get 'genre/:id', to: 'genre#show'
+      get 'genre', to: 'genre#index'
+
+      resource :torrent
+      get 'torrent/:id', to: 'torrent#show'
+      get 'torrent', to: 'torrent#index'
     end
   end
 end
