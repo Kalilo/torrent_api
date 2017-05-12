c9f0913d161bccf9e7771515cc1c6dd0fd33553f
Kalilo Jade Hansmann <kalilohansmann@gmail.com>
2017-05-12 10:30:39 +0200

    added linkage

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,7 @@ class Movie < ApplicationRecord
 
   has_many :torrents, through: :torrents_movies
   has_many :torrents_movies, dependent: :destroy
+
+  has_many :genres, through: :genres_movies
+  has_many :genres_movies, dependent: :destroy
 end
