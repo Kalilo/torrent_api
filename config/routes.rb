358adbac16de2ea99063a7486051c86d6f6ad08e
Kalilo Jade Hansmann <kalilohansmann@gmail.com>
2017-05-12 11:53:09 +0200

    Added routes for movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resource :movie
+      get 'movies/:id', to: 'movies#show'
+      get 'movies', to: 'movies#index'
+    end
+  end
 end
